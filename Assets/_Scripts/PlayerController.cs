7837c2a3e1e3a09672d88752bb35b82fa2819747
Keith Dreibelbis <kdreibel@gmail.com>
2024-03-20 12:36:33 -0700

    KEI-27 Add input action asset, basic left right movement.

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+}
